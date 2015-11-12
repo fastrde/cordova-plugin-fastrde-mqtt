bb291f640ecad05fb363784acb62b423d5c69968
Fabian Strachanski <fabian@fastr.de>
2015-11-12 23:05:19 +0100

    beautified

--- www/mqtt.js
+++ www/mqtt.js
@@ -1,54 +1,54 @@
 var mqtt = {
-	/**
+  /**
    * Paho MQTT client
    */
-	client: null,
+  client: null,
   /**
    *  @param {string} wsuri - full websocket uri with port and path for example ws://mymqttserver.io:8888/mqtt
    */ 
-	init: function(wsuri, clientId){
-		mqtt.client = new Paho.MQTT.Client(wsuri, clientId);
-	},
-	connect: function(connectOptions){
-		mqtt.client.connect(connectOptions);
-	},
-	disconnect: function(){
-		mqtt.client.disconnect();
-	},
-	getTraceLog: function(){
-		return mqtt.client.getTraceLog();
-	},
-	send: function(message){
-		mqtt.client.send(message);
-	},
-	startTrace: function(){
-		mqtt.client.startTrace();
-	},
-	stopTrace: function(){
-		mqtt.client.stopTrace();
-	},
-	subscribe: function(filter, subscribeOptions){
-		mqtt.client.subscribe(filter, subscribeOptions);
-	},
-	unsubscribe: function(filter, unsubscribeOptions){
-	 	mqtt.client.unsubscribe(filter, unsubscribeOptions);
-	},
+  init: function(wsuri, clientId){
+    mqtt.client = new Paho.MQTT.Client(wsuri, clientId);
+  },
+  connect: function(connectOptions){
+    mqtt.client.connect(connectOptions);
+  },
+  disconnect: function(){
+    mqtt.client.disconnect();
+  },
+  getTraceLog: function(){
+    return mqtt.client.getTraceLog();
+  },
+  send: function(message){
+    mqtt.client.send(message);
+  },
+  startTrace: function(){
+    mqtt.client.startTrace();
+  },
+  stopTrace: function(){
+    mqtt.client.stopTrace();
+  },
+  subscribe: function(filter, subscribeOptions){
+    mqtt.client.subscribe(filter, subscribeOptions);
+  },
+  unsubscribe: function(filter, unsubscribeOptions){
+    mqtt.client.unsubscribe(filter, unsubscribeOptions);
+  },
   createMessage: function(destination, payload, qos, retained){
-		var message = new Paho.MQTT.Message(payload);
-		message.destinationName = destination;
-		message.qos = qos || 0;
-		message.retained = retained || false;
-		return message;
-	},
-	setOnMessageArrived: function(func){
-		mqtt.client.onMessageArrived = func;
-	},
-	setOnMessageDelivered: function(func){
-		mqtt.client.onMessageDelivered = func;
-	},
-	setOnConnectionLost: function(func){
-		mqtt.client.onConnectionLost = func;
-	}
+    var message = new Paho.MQTT.Message(payload);
+    message.destinationName = destination;
+    message.qos = qos || 0;
+    message.retained = retained || false;
+    return message;
+  },
+  setOnMessageArrived: function(func){
+    mqtt.client.onMessageArrived = func;
+  },
+  setOnMessageDelivered: function(func){
+    mqtt.client.onMessageDelivered = func;
+  },
+  setOnConnectionLost: function(func){
+    mqtt.client.onConnectionLost = func;
+  }
 };
 
 module.exports = mqtt;
